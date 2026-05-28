0aa37ec0b6c746893408f3d415f71773cf2661f0
jackando33 <jand0048@student.monash.edu>
2026-05-28 18:14:09 +1000

    Add files via upload

---
+++ Data Vis 2/vega_lite_vis.js
@@ -0,0 +1,29 @@
+var vg_1 = "travel_chart_2.vg";
+vegaEmbed("#travel_chart", vg_1).catch(console.error);
+
+var vg_2 = "world_map_better.vg";
+vegaEmbed("#world_map", vg_2).catch(console.error);
+
+var vg_3 = "streamgraph.vg";
+vegaEmbed("#streamgraph", vg_3).catch(console.error);
+
+var vg_4 = "season_heatmap.vg";
+vegaEmbed("#heatmap", vg_4).catch(console.error);
+
+var vg_5 = "flow_map.vg";
+vegaEmbed("#flow_map", vg_5).catch(console.error);
+
+var vg_6 = "bubble_chart.vg";
+vegaEmbed("#bubble_chart", vg_6).catch(console.error);
+
+var vg_7 = "diverging_bar_chart.vg";
+vegaEmbed("#diverging_chart", vg_7).catch(console.error);
+
+var vg_8 = "top_10_countries_bar_vega.vg";
+vegaEmbed("#top10_chart", vg_8).catch(console.error);
+
+var vg_9 = "tree_map_interactive.vg"; 
+vegaEmbed("#tree_map", vg_9).catch(console.error);
+
+var vg_10 = "australia_choropleth.vg";
+vegaEmbed("#australia_choropleth", vg_10).catch(console.error);
